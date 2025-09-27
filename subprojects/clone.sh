93a77a6e8c635867d963c0c69fd36a27756d4971
Rhys Tumelty <rhys@tumelty.co.uk>
2025-09-27 17:43:24 +0100

    Added RmlUi submodule

--- subprojects/clone.sh
+++ subprojects/clone.sh
@@ -5,3 +5,4 @@ git clone https://github.com/skypjack/entt
 git clone https://github.com/libsdl-org/SDL --branch SDL2
 git clone https://github.com/libsdl-org/SDL_image --branch SDL2
 git clone https://github.com/libsdl-org/SDL_ttf --branch SDL2
+git clone https://github.com/mikke89/RmlUi
